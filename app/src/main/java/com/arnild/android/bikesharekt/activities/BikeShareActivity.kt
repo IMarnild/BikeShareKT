16e37b01a9d387b7f4fa182a95b2e3d29cfc2952
IMarnild <im.arnild@gmail.com>
2019-02-19 09:16:13 +0100

    saved list isShown state when rotating

--- app/src/main/java/com/arnild/android/bikesharekt/activities/BikeShareActivity.kt
+++ app/src/main/java/com/arnild/android/bikesharekt/activities/BikeShareActivity.kt
@@ -3,6 +3,7 @@ package com.arnild.android.bikesharekt.activities
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.ListView
 import com.arnild.android.bikesharekt.adapters.RideArrayAdapter
@@ -12,18 +13,21 @@ import com.arnild.android.bikesharekt.R
 class BikeShareActivity : AppCompatActivity() {
 
     private lateinit var listView: ListView
+    private val LIST_INDEX: String = "rideList"
+    private var isShown: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bike_share)
         this.listView = findViewById(R.id.list_view_ride)
+        isShown = savedInstanceState?.getBoolean(LIST_INDEX, false) ?: false
+        this.listView.visibility =  if (isShown) View.VISIBLE else View.GONE
     }
 
     override fun onStart() {
         super.onStart()
         val rideArrayAdapter = RideArrayAdapter(this, RideDb.getAll())
         this.listView.adapter = rideArrayAdapter
-        this.listView.visibility = View.GONE
     }
 
     fun startAddRide(view: View) {
@@ -37,8 +41,19 @@ class BikeShareActivity : AppCompatActivity() {
     }
 
     fun showRides(view: View) {
-        if (this.listView.visibility == View.GONE) this.listView.visibility = View.VISIBLE
-        else this.listView.visibility = View.GONE
+        if (this.listView.visibility == View.GONE) {
+            this.listView.visibility = View.VISIBLE
+            this.isShown = true
+        }
+        else {
+            this.listView.visibility = View.GONE
+            this.isShown = false
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState!!.putBoolean(LIST_INDEX, this.isShown)
     }
 }
 
